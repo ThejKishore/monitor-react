ff6f4443d01af9a26d72f5f2b19fbf7f8d756591
Abirami <abiramibcse@gmail.com>
2021-11-20 10:33:09 -0600

    adding new themes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import "primeicons/primeicons.css" //icons
 import "primereact/resources/primereact.min.css" //core css
-import "primereact/resources/themes/lara-light-indigo/theme.css" //theme
+// import "primereact/resources/themes/lara-light-indigo/theme.css" //theme
+// import "primereact/resources/themes/vela-green/theme.css" //theme
+import "primereact/resources/themes/tailwind-light/theme.css" //theme
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import App from './App'
 import ImportGroup from './features/imports/Imports'
-import './index.css'
 
  
 
